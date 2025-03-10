0090de5df230b0418900e8ebe607cf160db32ecd
Owais <abc@gmail.com>
2025-03-10 22:44:59 +0500

    add array for practising

--- js/app.js
+++ js/app.js
@@ -1,12 +1,10 @@
 // Qno1:
 
 // function power(a, b) {
-//     return a ** b; 
+//     return a ** b;
 // }
 
-
 // Qno:2
-
 
 // function isLeapYear(year) {
 //     return (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
@@ -15,31 +13,27 @@
 // const year = prompt("Enter a year:");
 // console.log(isLeapYear(year) ? "Leap Year" : "Not a Leap Year");
 
-
 // Qno:3
-
 
 // function calculateArea(a, b, c) {
 //     let S = (a + b + c) / 2;
 //     return Math.sqrt(S * (S - a) * (S - b) * (S - c));
 // }
 
-// console.log(calculateArea(3, 4, 5)); 
-
+// console.log(calculateArea(3, 4, 5));
 
 // Qno:4
-
 
 // function average(marks) {
 //     return marks.reduce((sum, mark) => sum + mark) / marks.length;
 // }
 
 // function percentage(marks) {
-//     return (average(marks) / 100) * 100; 
+//     return (average(marks) / 100) * 100;
 // }
 
 // function mainFunction() {
-//     const marks = [80, 90, 70]; 
+//     const marks = [80, 90, 70];
 //     const avg = average(marks);
 //     const perc = percentage(marks);
 //     console.log(`Average: ${avg}, Percentage: ${perc}%`);
@@ -47,10 +41,7 @@
 
 // mainFunction();
 
-
-
 // Qno:5
-
 
 // function customIndexOf(str, char) {
 //     for (let i = 0; i < str.length; i++) {
@@ -58,11 +49,10 @@
 //             return i;
 //         }
 //     }
-//     return -1; 
+//     return -1;
 // }
 
-// console.log(customIndexOf("hello", "e")); 
-
+// console.log(customIndexOf("hello", "e"));
 
 // Qno:6;
 
@@ -70,11 +60,9 @@
 //     return sentence.replace(/[aeiouAEIOU]/g, '');
 // }
 
-// console.log(removeVowels("Hello World")); 
-
+// console.log(removeVowels("Hello World"));
 
 // Qno:7
-
 
 // function countDoubleVowels(text) {
 //     let count = 0;
@@ -88,11 +76,9 @@
 //     return count;
 // }
 
-// console.log(countDoubleVowels("Pleases read this application and give me gratuity")); 
-
+// console.log(countDoubleVowels("Pleases read this application and give me gratuity"));
 
 // Qno:8;
-
 
 // function toMeters(km) {
 //     return km * 1000;
@@ -113,9 +99,7 @@
 // const distance = prompt("Enter distance in km:");
 // console.log(`Meters: ${toMeters(distance)}, Feet: ${toFeet(distance)}, Inches: ${toInches(distance)}, Centimeters: ${toCentimeters(distance)}`);
 
-
 // Qno:9;
-
 
 // function calculateOvertimePay(hoursWorked) {
 //     if (hoursWorked > 40) {
@@ -125,8 +109,7 @@
 //     return 0;
 // }
 
-// console.log(calculateOvertimePay(45)); 
-
+// console.log(calculateOvertimePay(45));
 
 // Qno:10;
 
@@ -145,3 +128,10 @@
 // const withdrawalAmount = prompt("Enter amount to withdraw in hundreds:");
 // const notes = currencyNotes(withdrawalAmount);
 // console.log(`100s: ${notes.notes100}, 50s: ${notes.notes50}, 10s: ${notes.notes10}`);
+
+
+let names = ['owais','ali','babar','aslam'];
+
+names.forEach(function(value){
+    console.log(value);
+})
